93123346ed8b7489759929add4a7eea9d812db76
Kevin L <av124773@gmail.com>
2024-05-08 17:39:07 +0800

    Add connection listening

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const net = require('net')
 const server = net.createServer()
 const port = 4001
 
+server.on('connection', (socket) => {
+    console.log('Got a new connection')
+})
+
 server.on('error', (err) => {
     console.log('Server error:', err.message)
 })
