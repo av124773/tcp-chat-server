9472aa3c5c937cf022d24a89d048c238ce5c5e8d
Kevin L <av124773@gmail.com>
2024-05-09 15:32:04 +0800

    feat: add user nickname in chat room

--- app.js
+++ app.js
@@ -6,24 +6,46 @@ const sockets = []
 
 server.on('connection', (socket) => {
     const userIp = `${socket.remoteAddress}:${socket.remotePort}`
-    console.log(`Got a new connection from ${userIp}`)
+    let userName = null
+    
+    let user = {
+        userIp,
+        socket,
+        userName
+    }
+    sockets.push(user)
 
-    sockets.push({ userIp, socket })
+    console.log(`Got a new connection from ${userIp}`)
+    socket.write('Please Set Your Nickname:')
 
     socket.on('data', (data) => {
         console.log('Got data:', data, 'from', userIp)
-        sockets.forEach(otherSocket => {
-            if (otherSocket.socket !== socket) {
-                otherSocket.socket.write(data)
-            }
-        })
+
+        if (!user.userName) {
+            const index = sockets.findIndex((s) => s.socket === socket )
+            userName = data.toString().trim()
+            sockets[index].userName = userName
+
+            socket.write(`Welcome ${userName}\n`)
+            broadcast(`${userName} join\n`)
+        } else {
+            broadcast(`${userName}:${data}`)
+        }
     })
 
     socket.on('end', () => {
         console.log(`${userIp} disconnected`)
-        const index = sockets.findIndex((sockets) => sockets.userIp === userIp) 
+        const index = sockets.findIndex((s) => s.userIp === userIp) 
         sockets.splice(index, 1)
     })
+
+    function broadcast(data) {
+        sockets.forEach(otherSocket => {
+            if (otherSocket.socket !== socket && otherSocket.userName) {
+                otherSocket.socket.write(data)
+            }
+        })
+    }
 })
 
 server.on('error', (err) => {
